ee1da8614e4cbf4156433bd5b46fba22fe34ae0c
Ryan Helinski <rlhelinski@gmail.com>
2018-01-15 10:36:20 -0700

    Finishing script. Need to add character colors and groupings (perhaps in a shared YAML file).

--- vizepisode.py
+++ vizepisode.py
@@ -2,6 +2,9 @@ import argparse
 import yaml
 from graphviz import Digraph
 
+def keyify(name):
+    return name.replace('.', '').replace(' ', '_')
+
 parser = argparse.ArgumentParser(
     description='Take a YAML file representing an episode '
                 'and translate it to a Graphviz DOT file '
@@ -13,13 +16,50 @@ for filename in args.input:
     with open(filename, 'r') as yaml_f:
         episode = yaml.load(yaml_f)
 
-    dot = Digraph(comment=episode['title'])
+    epoch_keys = []
+    character_keys = {}
 
-    timeline = Digraph()
+    dot = Digraph(comment=episode['title'])
+    dot.attr(center='1', randir='TB')
+    dot.attr('edge', dir='none')
+    dot.attr('node', width='0.3', height='0.3')
+
+    timeline = Digraph(name='timeline')
+    timeline.attr('node', style='invis', label='')
+    timeline.attr('edge', style='invis')
     for epoch in episode['scenes']:
         for key in epoch.keys():
-            timeline.node(key.replace(' ', '_'))
+            key = keyify(key)
+            epoch_keys.append(key)
+            timeline.node(key)
     dot.subgraph(timeline)
+
+    for epoch in episode['scenes']:
+        for key, scenes in epoch.items():
+            key = keyify(key)
+            epoch = Digraph(name='cluster_%s' % key)
+            for i, scene in enumerate(scenes):
+                scene_key = '%s_%d' % (key, i) 
+                scene_cluster = Digraph(name='cluster_%s' % scene_key)
+                scene_cluster.attr(label='%s: %s' % (scene['location'], scene['title']))
+                if 'characters' in scene:
+                    for character in scene['characters']:
+                        if character not in character_keys:
+                            character_keys[character] = []
+                        character_key = keyify(character)+'_'+scene_key
+                        character_keys[character].append(character_key)
+                        scene_cluster.node(character_key)
+                epoch.subgraph(scene_cluster)
+            dot.subgraph(epoch)
+
+    for character_name, character_key_list in character_keys.items():
+        character_cluster = Digraph()
+        character_cluster.attr(label=character_name)
+        for character_key in character_key_list:
+            character_cluster.node(character_key)
+        dot.subgraph(character_cluster)
+
+    # finish here
 
     with open(filename.replace('.yml', '-test.dot'), 'w') as dot_f:
         dot_f.write(str(dot))
